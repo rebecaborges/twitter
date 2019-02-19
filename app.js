25220e23d071224d18d95f23e12276e2d39bd80c
rebecaborges <rebeca.felix@hotmail.com>
2019-02-19 13:12:20 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function saveText(evt){
+    let saveMessage = document.getElementById('message').value;
+    let saveShowMessage = document.getElementById('showMessage');
+
+    saveShowMessage.innerHTML=saveMessage;
+
+    evt.preventDefault()
+}
+
+let button = document.getElementById('buttonTweet')
+button.addEventListener('click', saveText)
+
+
+
+
