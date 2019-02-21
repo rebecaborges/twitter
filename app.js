341068a54a6f22b7d8312f33ab9a8b8ef25d791b
rebecaborges <rebeca.felix@hotmail.com>
2019-02-20 23:30:27 -0300

    implementei contador e uma parte do css

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
+let saveMessage = document.getElementById('message');
+let saveShowMessage = document.getElementById('showMessage');
+let button = document.getElementById('buttonTweet')
+let counter = document.getElementById('counter');
 
-function saveText(evt){
-    let saveMessage = document.getElementById('message').value;
-    let saveShowMessage = document.getElementById('showMessage');
-
-    saveShowMessage.innerHTML=saveMessage;
+function showText(evt){
+    saveShowMessage.innerHTML=saveMessage.value;
 
     evt.preventDefault()
 }
 
-let button = document.getElementById('buttonTweet')
-button.addEventListener('click', saveText)
+button.addEventListener('click', showText)
 
 
+// function enableButton (evt){
+//     if(saveMessage ===''){
+//     button.disabled=true
+//     }else{button.disabled=false}  
 
+//     evt.preventDefault();
 
+// }
+// saveMessage.addEventListener('click', enableButton)
+
+function sumCaracter(saveMessage){
+    let max = 140;
+    let msg = saveMessage.value.length
+    let total = max-msg
+
+    if(total < 0){
+        button.disabled=true;   
+           console.log(button)
+    }else{
+        button.disabled=false;
+    }
+    counter.innerHTML= total
+}
+
+function showCaracter (evt){
+    sumCaracter(saveMessage)
+    evt.preventDefault()
+}
+saveMessage.addEventListener('keyup', showCaracter)
